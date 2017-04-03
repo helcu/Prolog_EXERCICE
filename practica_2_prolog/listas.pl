60fb7ab02b0b5272e590bde4802bd47be05dc87f
Alumnos <Alumnos>
2017-04-03 10:56:02 -0500

    ejercicios con listas

---
+++ practica_2_prolog/listas.pl
@@ -0,0 +1,8 @@
+lista1:-
+    Numeros=[1,2,3,4,5,6,7,8,9],
+    writef('elementos de la lista son: %w',[Numeros]).
+
+lista2:-
+    Nombres=[helmut,pepe,lusho],
+    writef('elementos de la lista son: %w',[Nombres]).
+
